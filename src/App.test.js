199c84fc71e4b6de98a24f9addb6eb2f8629da69
UrbanWill <47801291+UrbanWill@users.noreply.github.com>
2019-12-29 14:13:51 +0900

    Started testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+it("renders without crashing", () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
 });
+
+it("renders increment button", () => {});
+
+it("renders counter display", () => {});
+
+it("renders counter display starts at zero", () => {});
+
+it("renders clicking button increments counter display", () => {});
